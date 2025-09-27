21c878deeba2ffafcac15725b7c4a8eac8acdde6
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-09-27 11:27:49 +0300

    feat: Добавлен расширяющий метод для добавления сервисов из Application. Добавлен MediatR в DI контейнер

---
+++ AuthService.Application/Extensions/DependencyInjectionConfiguration.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthService.Application.Extensions;
+
+public static class DependencyInjectionConfiguration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjectionConfiguration).Assembly));
+
+        return services;
+    }
+}
