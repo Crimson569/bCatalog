8745daba4d4a311404a8612dca0ca8279dfe1d92
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-03 21:35:33 +0300

    refactor: добавлено поле Id в UserDto

--- AuthService.Application/Dto/UserDto.cs
+++ AuthService.Application/Dto/UserDto.cs
@@ -2,4 +2,4 @@ using AuthService.Domain.Entities;
 
 namespace AuthService.Application.Dto;
 
-public record UserDto(string Username, string UserEmail, IReadOnlyCollection<Role> Roles);
+public record UserDto(Guid Id, string Username, string UserEmail, IReadOnlyCollection<Role> Roles);
