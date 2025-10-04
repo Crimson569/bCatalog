0d4a2d6bf300b1bbcc1dbf03c292823501d235a1
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-04 09:47:13 +0300

    feat: Действия над пользователями вынесены из AuthController в UserController

--- AuthService.Api/Controllers/AuthController.cs
+++ AuthService.Api/Controllers/AuthController.cs
@@ -17,22 +17,6 @@ public class AuthController : ControllerBase
         _mediator = mediator;
     }
     
-    [HttpGet]
-    [Route("users")]
-    public async Task<ActionResult> GetAllUsersAsync(CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(new GetUsersRequest(), cancellationToken);
-        return Ok(result);
-    }
-
-    [HttpGet]
-    [Route("users/{id:guid}")]
-    public async Task<ActionResult> GetUserByIdAsync(Guid id, CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(new GetUserByIdRequest(){UserId = id}, cancellationToken);
-        return Ok(result);
-    }
-
     [HttpPost]
     [Route("users")]
     public async Task<ActionResult> CreateUserAsync([FromBody] UserCreateDto userDto, CancellationToken cancellationToken)
@@ -56,55 +40,6 @@ public class AuthController : ControllerBase
         CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(new LoginUserWithUserNameCommand() { UserDto = userDto }, cancellationToken);
-        return Ok(result);
-    }
-
-    [HttpPost]
-    [Route("users/{userId:guid}/roles")]
-    public async Task<ActionResult> AddRoleToUserAsync(Guid userId, [FromBody] UserAddRoleDto userAddRoleDto,
-        CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(
-            new AddRoleToUserCommand()
-            {
-                UserId = userId,
-                UserAddRoleDto = userAddRoleDto
-            }, 
-            cancellationToken);
-
-        return Ok(result);
-    }
-    
-    [HttpDelete]
-    [Route("users/{userId:guid}/roles")]
-    public async Task<ActionResult> RemoveRoleFromUserAsync(Guid userId, [FromBody] UserRemoveRoleDto userRemoveRoleDto,
-        CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(
-            new RemoveRoleFromUserCommand()
-            {
-                UserId = userId,
-                UserRemoveRoleDto = userRemoveRoleDto
-            }, 
-            cancellationToken);
-
-        return Ok(result);
-    }
-    
-    [HttpPut]
-    [Route("users/{id:guid}")]
-    public async Task<ActionResult> UpdateUserAsync([FromBody] UserUpdateDto userDto,
-        CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(new UpdateUserCommand() { UserDto = userDto }, cancellationToken);
-        return Ok(result);
-    }
-
-    [HttpDelete]
-    [Route("users/{id:guid}")]
-    public async Task<ActionResult> DeleteUserByIdAsync(Guid id, CancellationToken cancellationToken)
-    {
-        var result = await _mediator.Send(new DeleteUserCommand() { UserId = id }, cancellationToken);
         return Ok(result);
     }
 }
---
+++ AuthService.Api/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+using AuthService.Application.Dto;
+using AuthService.Application.Features.Users.Requests.Commands;
+using AuthService.Application.Features.Users.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    [Route("users")]
+    public async Task<ActionResult> GetAllUsersAsync(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetUsersRequest(), cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("users/{id:guid}")]
+    public async Task<ActionResult> GetUserByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetUserByIdRequest(){UserId = id}, cancellationToken);
+        return Ok(result);
+    }
+    
+    [HttpPost]
+    [Route("users/{userId:guid}/roles")]
+    public async Task<ActionResult> AddRoleToUserAsync(Guid userId, [FromBody] UserAddRoleDto userAddRoleDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(
+            new AddRoleToUserCommand()
+            {
+                UserId = userId,
+                UserAddRoleDto = userAddRoleDto
+            }, 
+            cancellationToken);
+
+        return Ok(result);
+    }
+    
+    [HttpDelete]
+    [Route("users/{userId:guid}/roles")]
+    public async Task<ActionResult> RemoveRoleFromUserAsync(Guid userId, [FromBody] UserRemoveRoleDto userRemoveRoleDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(
+            new RemoveRoleFromUserCommand()
+            {
+                UserId = userId,
+                UserRemoveRoleDto = userRemoveRoleDto
+            }, 
+            cancellationToken);
+
+        return Ok(result);
+    }
+    
+    [HttpPut]
+    [Route("users/{id:guid}")]
+    public async Task<ActionResult> UpdateUserAsync([FromBody] UserUpdateDto userDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new UpdateUserCommand() { UserDto = userDto }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    [Route("users/{id:guid}")]
+    public async Task<ActionResult> DeleteUserByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new DeleteUserCommand() { UserId = id }, cancellationToken);
+        return Ok(result);
+    }
+}
