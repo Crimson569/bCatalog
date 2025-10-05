3191fe222784d9c5ef91d2adc8d1d4c7aad1e545
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-05 21:39:20 +0300

    feat: Создан и реализован PermissionController

---
+++ AuthService.Api/Controllers/PermissionController.cs
@@ -0,0 +1,66 @@
+using AuthService.Application.Dto;
+using AuthService.Application.Features.Permissions.Requests.Commands;
+using AuthService.Application.Features.Permissions.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PermissionController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PermissionController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("permissions")]
+    public async Task<ActionResult> GetAllPermissionsAsync(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetPermissionsRequest(), cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("permissions/{id:guid}")]
+    public async Task<ActionResult> GetPermissionByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetPermissionByIdRequest() { PermissionId = id }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("permissions")]
+    public async Task<ActionResult> CreatePermissionAsync([FromBody] PermissionCreateUpdateDto permissionDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new CreatePermissionRequest() { PermissionDto = permissionDto },
+            cancellationToken);
+
+        return Ok(result); //Заменить на Created
+    }
+
+    [HttpPut]
+    [Route("permissions/{id:guid}")]
+    public async Task<ActionResult> UpdatePermissionAsync(Guid id, [FromBody] PermissionCreateUpdateDto permissionDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(
+            new UpdatePermissionRequest() { PermissionId = id, PermissionDto = permissionDto },
+            cancellationToken);
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    [Route("permissions/{id:guid}")]
+    public async Task<ActionResult> DeletePermissionAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new DeletePermissionRequest() { PermissionId = id }, cancellationToken);
+        return Ok(result);
+    }
+}
