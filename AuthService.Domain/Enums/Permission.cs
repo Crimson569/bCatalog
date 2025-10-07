20bf2acd11eff862c47896dbfce57081998fef29
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-07 18:07:45 +0300

    feat: Созданы перечисления Role и Permission

---
+++ AuthService.Domain/Enums/Permission.cs
@@ -0,0 +1,9 @@
+namespace AuthService.Domain.Enums;
+
+public enum Permission
+{
+    PostBook = 1,
+    ReadBook = 2,
+    UpdateBook = 3,
+    DeleteBook = 4
+}
---
+++ AuthService.Domain/Enums/Role.cs
@@ -0,0 +1,7 @@
+namespace AuthService.Domain.Enums;
+
+public enum Role
+{
+    Admin = 1,
+    Reader = 2
+}
