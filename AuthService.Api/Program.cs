1da61de46ab1d97d0ab831a7b4071e301e53af8c
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-07 18:33:00 +0300

    feat: Создан и сконфигурирован класс AutorizationOptions

--- AuthService.Api/Program.cs
+++ AuthService.Api/Program.cs
@@ -2,6 +2,7 @@ using AuthService.Api.Extensions;
 using AuthService.Application.Extensions;
 using AuthService.Infrastructure.Extensions;
 using AuthService.Infrastructure.Options;
+using AuthService.Persistence;
 using AuthService.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
+builder.Services.Configure<AuthorizationOptions>(builder.Configuration.GetSection("AuthorizationOptions"));
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
