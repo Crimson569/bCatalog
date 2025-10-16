74480e9397ec351741922b7b1f889c8a5253f3fe
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-16 20:54:50 +0300

    feat: Создан Shared-проект, добавлен интерфейс IFileService для работы с файлами

---
+++ Shared/Shared/Interfaces/IFileService.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Shared.Interfaces;
+
+public interface IFileService
+{
+    Task<string> SaveFileAsync(IFormFile file, string directory);
+    Task<Stream> GetFileAsync(string path);
+    Task DeleteFileAsync(string path);
+    Task<bool> FileExistsAsync(string path);
+    Task<string> UpdateFileAsync(IFormFile newFile, string oldFilePath);
+}
