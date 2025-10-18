f8b6563c0cb5dfa4b5fedc0992a3775b6f468361
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-18 15:39:16 +0300

    fix: фикс параметра directory в методе SaveFileAsync интерфейса IFileService

--- Shared/Shared/Interfaces/IFileService.cs
+++ Shared/Shared/Interfaces/IFileService.cs
@@ -4,7 +4,7 @@ namespace Shared.Interfaces;
 
 public interface IFileService
 {
-    Task<string> SaveFileAsync(IFormFile file, string? directory);
+    Task<string> SaveFileAsync(IFormFile file, string? directory = null);
     Task<Stream> GetFileAsync(string path);
     Task DeleteFileAsync(string path);
     Task<bool> FileExistsAsync(string path);
