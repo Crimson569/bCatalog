e99e0889502cbce2e4f1c791a592ea8fa0644669
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-20 22:17:15 +0300

    feat: Доделана валидация UserCreateDto и UserUpdateDto

--- AuthService.Api/Validators/Users/UserCreateDtoValidator.cs
+++ AuthService.Api/Validators/Users/UserCreateDtoValidator.cs
@@ -1,4 +1,7 @@
+using System.Text.RegularExpressions;
 using AuthService.Application.Dto.UserDataTransferObjects;
+using AuthService.Application.Primitives.Patterns;
+
 using FluentValidation;
 
 namespace AuthService.Api.Validators.Users;
@@ -8,13 +11,20 @@ public class UserCreateDtoValidator : AbstractValidator<UserCreateDto>
     public UserCreateDtoValidator()
     {
         RuleFor(u => u.Username)
+            .NotEmpty().WithMessage("Username is required.")
             .MaximumLength(50)
             .MinimumLength(3);
 
-        RuleFor(u => u.UserEmail);
+        RuleFor(u => u.UserEmail)
+            .NotEmpty().WithMessage("Email is required.")
+            .Matches(RegexPatterns.Email())
+            .WithMessage("Invalid email format.");
 
         RuleFor(u => u.Password)
+            .NotEmpty().WithMessage("Password is required.")
             .MaximumLength(20)
-            .MinimumLength(7);
+            .MinimumLength(7)
+            .Matches(RegexPatterns.Password())
+            .WithMessage("Invalid password format.");
     }
 }
--- AuthService.Api/Validators/Users/UserUpdateDtoValidator.cs
+++ AuthService.Api/Validators/Users/UserUpdateDtoValidator.cs
@@ -1,4 +1,5 @@
 using AuthService.Application.Dto.UserDataTransferObjects;
+using AuthService.Application.Primitives.Patterns;
 using FluentValidation;
 
 namespace AuthService.Api.Validators.Users;
@@ -8,17 +9,27 @@ public class UserUpdateDtoValidator : AbstractValidator<UserUpdateDto>
     public UserUpdateDtoValidator()
     {
         RuleFor(u => u.Username)
+            .NotEmpty().WithMessage("Username is required.")
             .MaximumLength(50)
             .MinimumLength(3);
         
-        RuleFor(u => u.UserEmail);
+        RuleFor(u => u.UserEmail)
+            .NotEmpty().WithMessage("Email is required.")
+            .Matches(RegexPatterns.Email())
+            .WithMessage("Invalid email format.");
         
         RuleFor(u => u.OldPassword)
+            .NotEmpty().WithMessage("Old password is required.")
             .MaximumLength(20)
-            .MinimumLength(7);
+            .MinimumLength(7)
+            .Matches(RegexPatterns.Password())
+            .WithMessage("Invalid password format.");
         
         RuleFor(u => u.NewPassword)
+            .NotEmpty().WithMessage("New password is required.")
             .MaximumLength(20)
-            .MinimumLength(7);
+            .MinimumLength(7)
+            .Matches(RegexPatterns.Password())
+            .WithMessage("Invalid password format.");
     }
 }
