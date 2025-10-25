f1cb637a9865bd35138831dc67fcbe2243ec1bd7
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2025-10-25 16:31:07 +0300

    feat: реализована инициализация и освобождение ресурсов для контейнеров PostgreSQL и Minio

--- AuthService/Tests/AuthService.Integration/Helpers/ApplicationFactory.cs
+++ AuthService/Tests/AuthService.Integration/Helpers/ApplicationFactory.cs
@@ -1,6 +1,11 @@
+using AuthService.Infrastructure.Options;
+using AuthService.Persistence;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Minio;
+using Minio.AspNetCore;
 using Testcontainers.Minio;
 using Testcontainers.PostgreSql;
 
@@ -16,7 +21,14 @@ public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     private const string MinioImage = "minio/minio:latest";
     private const string MinioUsername = "minio_user";
     private const string MinioPassword = "minio_password";
+    
+    private IServiceScope _scope = null!;
+    private IMinioClient _minioClient = null!;
+    public string _minioBucket = null!;
 
+    public AuthServiceDbContext Context { get; private set; } = null!;
+    public HttpClient HttpClient { get; private set; } = null!;
+    
     private readonly PostgreSqlContainer _postgresContainer = new PostgreSqlBuilder()
         .WithImage(PostgresImage)
         .WithDatabase(DbName)
@@ -30,16 +42,24 @@ public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
         .WithPassword(MinioPassword)
         .Build();
     
-    public Task InitializeAsync()
+    public async Task InitializeAsync()
     {
-        throw new NotImplementedException();
+        await Task.WhenAll(_postgresContainer.StartAsync(), _minioContainer.StartAsync());
+        _scope = Services.CreateScope();
+
+        _minioClient = _scope.ServiceProvider.GetRequiredService<IMinioClient>();
+        _minioBucket = _scope.ServiceProvider.GetRequiredService<IOptions<BucketOptions>>().Value.Name;
+
+        Context = _scope.ServiceProvider.GetRequiredService<AuthServiceDbContext>();
+
+        HttpClient = CreateClient();
+
+        await Context.Database.MigrateAsync();
     }
 
-    public Task DisposeAsync()
+    public new async Task DisposeAsync()
     {
-        throw new NotImplementedException();
+        await _postgresContainer.DisposeAsync();
+        await _minioContainer.DisposeAsync();
     }
-
-    private IServiceScope _scope = null!;
-    private IMinioClient _minioClient = null!;
 }
